a5e98949cfc94f84d60ab74c97e4e6da37af7d2c
Roker2 <dimaminko2000@gmail.com>
2021-02-28 06:08:56 +0300

    s2: fingerprint: Remove FPC code
    s2 doesn't have FPC scanner
    
    Change-Id: I78143792e678badda8ec5f2763e81db5528c9e6e

--- fingerprint/gdxbiometrics/service.cpp
+++ fingerprint/gdxbiometrics/service.cpp
@@ -25,7 +25,6 @@
 #include <android/hardware/biometrics/fingerprint/2.1/types.h>
 
 #include "BiometricsFingerprint.h"
-#include <cutils/properties.h>
 #include <binder/ProcessState.h>
 #include <errno.h>
 #include <unistd.h>
@@ -36,35 +35,20 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 using android::sp;
 
-bool is_goodix = false;
-
 static constexpr char kGoodixFpDev[] = "/dev/goodix_fp";
 
 int main() {
-    char vend[PROPERTY_VALUE_MAX];
-    property_get("ro.hardware.fingerprint", vend, "none");
-
-    if (!strcmp(vend, "none")) {
-        ALOGE("ro.hardware.fingerprint not set! Killing " LOG_TAG " binder service!");
-        return 1;
-    } else if (!strcmp(vend, "goodix")) {
-        ALOGI("is_goodix = true");
-        is_goodix = true;
-    }
-
     ALOGI("Start biometrics");
     android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
 
-    if (is_goodix) {
-        if (access(kGoodixFpDev, F_OK) != 0) {
-            ALOGE("Cannot access %s (%s)", kGoodixFpDev, strerror(errno));
-            return 1;
-        }
-
-        // the conventional HAL might start binder services
-        android::ProcessState::initWithDriver("/dev/binder");
-        android::ProcessState::self()->startThreadPool();
+    if (access(kGoodixFpDev, F_OK) != 0) {
+        ALOGE("Cannot access %s (%s)", kGoodixFpDev, strerror(errno));
+        return 1;
     }
+
+    // the conventional HAL might start binder services
+    android::ProcessState::initWithDriver("/dev/binder");
+    android::ProcessState::self()->startThreadPool();
 
     /* process Binder transaction as a single-threaded program. */
     configureRpcThreadpool(1, true /* callerWillJoin */);
